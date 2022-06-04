1c7cb5d8330bff10391f44906c269477a6dfca97
loctan81 <locvtse141035@fpt.edu.vn>
2022-06-04 23:45:50 +0700

    loctan update

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Loc Tan
+          Loc Tan ahihi
         </p>
         <a
           className="App-link"
